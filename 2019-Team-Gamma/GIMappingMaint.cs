9f44c468c6269fb0b9536f02ba41481ca947bf84
Vladimir Perov <vperov@acumatica.com>
2019-01-26 21:11:22 -0600

    selectors working on form header + graph type defaulting

--- 2019-Team-Gamma/GIMappingMaint.cs
+++ 2019-Team-Gamma/GIMappingMaint.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using PX.Data;
 
 namespace PowerTabs
@@ -10,8 +11,19 @@ namespace PowerTabs
 		  Where<GIMappingLine.screenID, Equal<Current<GIMapping.screenID>>, 
 			  And<GIMappingLine.designID, Equal<Current<GIMapping.designID>>>>> MappingLines;
 
-	  #region Event Handlers
+		#region Event Handlers
+
+	  protected virtual void _(Events.FieldUpdated<GIMapping, GIMapping.screenID> e)
+	  {
+		  if (!String.IsNullOrEmpty(e.Row.ScreenID))
+		  {
+			  GIMappingPreset graphName = PXSelect<GIMappingPreset, 
+				  Where<GIMappingPreset.screenID, Equal<Required<GIMappingPreset.screenID>>>>.Select(this, e.Row.ScreenID);
+
+			  e.Cache.SetValueExt<GIMapping.graphTypeName>(e.Row, graphName?.GraphTypeName);
+		  }
+	  }
 
 	  #endregion
-  }
+	}
 }
--- UI/Pages/PT/PT200000.aspx
+++ UI/Pages/PT/PT200000.aspx
@@ -15,8 +15,8 @@
 	<px:PXFormView ID="form" runat="server" DataSourceID="ds" DataMember="Mapping" Width="100%" Height="100px" AllowAutoHide="false">
 		<Template>
 			<px:PXLayoutRule ID="PXLayoutRule1" runat="server" StartRow="True"></px:PXLayoutRule>
-			<px:PXSelector runat="server" ID="CstPXSelector3" DataField="ScreenID" ></px:PXSelector>
-			<px:PXSelector runat="server" ID="CstPXSelector4" DataField="DesignID" /></Template>
+			<px:PXSelector CommitChanges="True" runat="server" ID="CstPXSelector3" DataField="ScreenID" ></px:PXSelector>
+			<px:PXSelector CommitChanges="True" runat="server" ID="CstPXSelector4" DataField="DesignID" ></px:PXSelector></Template>
 	</px:PXFormView>
 </asp:Content>
 <asp:Content ID="cont3" ContentPlaceHolderID="phG" Runat="Server">
